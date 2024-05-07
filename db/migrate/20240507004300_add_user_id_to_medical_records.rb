b590788a361e0e24110dbfd719f66226e0e49ad9
NadiaArroyo25 <niac2596@gmail.com>
2024-05-07 15:41:21 -0600

    modify
    Well, I already modified the migration, it didn't work at first, but not because of the password, it already worked, so I went back to my previous queries and it now works.

--- db/migrate/20240507004300_add_user_id_to_medical_records.rb
+++ db/migrate/20240507004300_add_user_id_to_medical_records.rb
@@ -1,11 +1,9 @@
 class AddUserIdToMedicalRecords < ActiveRecord::Migration[7.1]
   def change
-    user = User.first || User.create(email: "temporary_email@example.com", password: "pass", password_confirmation: "pass")
+    user = Usuario.first || Usuario.create(email: "temporary_email@example.com", password: "atemporal", password_confirmation: "atemporal")
     add_reference :medical_records, :usuario, null: true, foreign_key: true, default: user.id
+
     change_column_default :medical_records, :usuario_id, nil
-
-
-    change_column_null :medical_records, :usuario_id, false
 
   end
 end
