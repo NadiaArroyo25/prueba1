064fda74d15c83613d62bc51a8f127274b4f6ba2
NadiaArroyo25 <niac2596@gmail.com>
2024-05-17 16:32:48 -0600

    add column phone number
    It is a small change to the patient's data, having a phone number in the database is necessary for appointment reminders or things like that.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_08_233721) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_221334) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_233721) do
     t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "state", default: "scheduled"
   end
 
   create_table "articulos", force: :cascade do |t|
@@ -103,6 +104,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_08_233721) do
     t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
   end
 
   create_table "users", force: :cascade do |t|
